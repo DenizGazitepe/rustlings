0762621475a206eeff2129779392c1609143df31
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2024-08-21 17:11:38 +0200

    fix(exercises): ✅ pass traits5

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -19,12 +19,16 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
 fn main() {
     // You can optionally experiment here.
+    let some_struct = SomeStruct;
+    let other_struct = OtherStruct;
+    some_func(some_struct);
+    some_func(other_struct);
 }
 
 #[cfg(test)]
